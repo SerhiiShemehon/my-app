627cb904a9ea1c9080b7a83b0bb62af642e9cee0
SerhiiShemehon <sergei.shemegon.ua@gmail.com>
2023-01-04 23:38:29 +0200

    updated homework #3

--- src/components/DashboardSidebar/index.js
+++ src/components/DashboardSidebar/index.js
@@ -1,15 +1,17 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext} from 'react';
 import Loading from 'components/Loading';
 import {NavItem, NavList} from 'components/Nav';
-import {MENU_DASHBOARD, USERS} from 'data';
+import { UserContext } from 'context/userContext';
+import {MENU_DASHBOARD} from 'data';
 
 const AdminInfo = React.lazy(()=> import('components/AdminInfo'));
 
 const DashboardSidebar = () => {
+  const user = useContext(UserContext);
   return (
     <div className={'dashboard-sidebar'}>
       <Suspense fallback={<Loading />}>
-        <AdminInfo user={USERS[0]} />
+        <AdminInfo user={user} />
       </Suspense>
       <NavList className={'dashboard-menu'}>
         {MENU_DASHBOARD.map((item, index) => (
